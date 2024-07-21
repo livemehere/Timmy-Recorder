50949a945000a11255a91bc574e1374e5d8b5b86
livemehere <rhdxoals@gmail.com>
2024-07-21 23:18:58 +0900

    :sparkles: 편집페이지 추가

--- modules/renderer/src/pages/VideoEditor.tsx
+++ modules/renderer/src/pages/VideoEditor.tsx
@@ -1,3 +1,10 @@
+import Container from '../components/ui/Container';
+import Title from '@renderer/src/components/ui/Title';
+
 export default function VideoEditor() {
-  return <div>VideoEditor</div>;
+  return (
+    <Container>
+      <Title>비디오 편집</Title>
+    </Container>
+  );
 }
