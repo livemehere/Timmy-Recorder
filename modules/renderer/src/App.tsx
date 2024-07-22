b4e9ee5f7d5f9343d4efe6439b226363617f3a3d
taemin_Kong <rhdxoals@nexon.co.kr>
2024-07-22 20:33:07 +0900

    Add : tanstack query

--- modules/renderer/src/App.tsx
+++ modules/renderer/src/App.tsx
@@ -9,24 +9,42 @@ import { NextUIProvider } from '@nextui-org/react';
 import Settings from '@renderer/src/pages/Settings';
 import AutoRecord from '@renderer/src/pages/AutoRecord';
 import VideoEditor from '@renderer/src/pages/VideoEditor';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const App = () => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+            gcTime: 1000 * 60 * 2,
+            staleTime: 1000 * 60
+          }
+        }
+      })
+  );
   return (
-    <NextUIProvider>
-      <HashRouter>
-        <DevTools />
-        <ModalProvider>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<ManualRecord />} />
-              <Route path="/auto-record" element={<AutoRecord />} />
-              <Route path="/video-editor" element={<VideoEditor />} />
-              <Route path="/settings" element={<Settings />} />
-            </Routes>
-          </Layout>
-        </ModalProvider>
-      </HashRouter>
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <NextUIProvider>
+        <HashRouter>
+          <DevTools />
+          <ModalProvider>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<ManualRecord />} />
+                <Route path="/auto-record" element={<AutoRecord />} />
+                <Route path="/video-editor" element={<VideoEditor />} />
+                <Route path="/settings" element={<Settings />} />
+              </Routes>
+            </Layout>
+          </ModalProvider>
+        </HashRouter>
+      </NextUIProvider>
+    </QueryClientProvider>
   );
 };
 
